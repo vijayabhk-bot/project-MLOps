8e00873e603b745344ff51104e38ee027e4f7a81
Jashuva-Billa <jashu1150@gmail.com>
2025-09-09 02:47:41 +0530

    added ml flow

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
+# from src.data_preprocessing import load_and_preprocess_data
+# from src.model_training import train_model
+# from src.model_evaluation import evaluate_model
+
+# def main():
+#     X_train, X_test, y_train, y_test = load_and_preprocess_data()
+#     model = train_model(X_train, y_train)
+#     evaluate_model(model, X_test, y_test)
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+
+import mlflow
+import mlflow.sklearn
 from src.data_preprocessing import load_and_preprocess_data
 from src.model_training import train_model
 from src.model_evaluation import evaluate_model
 
 def main():
-    X_train, X_test, y_train, y_test = load_and_preprocess_data()
-    model = train_model(X_train, y_train)
-    evaluate_model(model, X_test, y_test)
+    mlflow.set_experiment("/Shared/fraud-detection-exp")
+
+    with mlflow.start_run():
+        X_train, X_test, y_train, y_test = load_and_preprocess_data()
+        model = train_model(X_train, y_train)
+        report = evaluate_model(model, X_test, y_test)
+
+        # Log model
+        mlflow.sklearn.log_model(model, "fraud_model")
+        mlflow.log_param("algorithm", "LogisticRegression")
 
 if __name__ == "__main__":
     main()
--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,8 +1,22 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+import os
+
+
+def resolve_path(path):
+    # If running locally, fallback to relative data folder
+    if not os.path.exists(path):
+        if path.startswith("dbfs:/"):
+            # Convert dbfs:/ to /dbfs/ for Databricks
+            path = path.replace("dbfs:/", "/dbfs/")
+        elif "creditcard.csv" in path:
+            # Use local dataset
+            path = os.path.join("data", "creditcard.csv")
+    return path
 
 def load_and_preprocess_data(path="data/creditcard.csv"):
+    path = resolve_path(path)
     df = pd.read_csv(path)
     
     # Features & labels
